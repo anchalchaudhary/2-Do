77e694ff6e9095470c67d86a592a3cf15b1db2ac
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-07-11 01:41:09 +0530

    View, mark as completed, delete task added

---
+++ Controllers/MyTasksController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class MyTasksController : Controller
+    {
+        DBToDoListEntities db = new DBToDoListEntities();
+        // GET: MyTasks
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //public ActionResult ViewTask()
+        //{
+        //    return View();
+        //}
+        public ActionResult ViewTask(Task model)
+        {
+            int loggedinUserID = Convert.ToInt32(Session["UserID"]);
+            List<Task> taskList = db.tblTasks.Where(x => x.Completed == 0 && x.ID == loggedinUserID).Select(x => new Task
+            {
+                TaskID = x.TaskID,
+                Title = x.Title,
+                TaskDetail = x.TaskDetail,
+                StartDate = x.StartDate,
+                StartTime = x.StartTime,
+                EndDate = x.EndDate,
+                EndTime = x.EndTime
+            }).ToList();
+
+            ViewBag.TaskList = taskList;
+
+            return View();
+        }
+        public ActionResult ViewTaskDetails(int TaskID)
+        {
+            List<Task> requiredTaskDetails = db.tblTasks.Where(x => x.TaskID == TaskID).Select(x => new Task
+            {
+                Title = x.Title,
+                TaskDetail = x.TaskDetail,
+                StartDate = x.StartDate,
+                StartTime = x.StartTime,
+                EndDate = x.EndDate,
+                EndTime = x.EndTime
+            }).ToList();
+
+            ViewBag.RequiredTaskDetails = requiredTaskDetails;
+
+            return PartialView("TaskDetailsPartial");
+        }
+    }
+}
