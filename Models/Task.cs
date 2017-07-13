74a9848a0b21af0637ffc95991e1cb7ef4659153
Anchal Chaudhary <anchal.ldn5@gmail.com>
2017-07-13 17:01:35 +0530

    Validations

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,13 +10,17 @@ namespace ToDoList.Models
     {
         public int TaskID { get; set; }
         public Nullable<int> ID { get; set; }
+        [Required(ErrorMessage = "Enter Title")]
         public string Title { get; set; }
         public string TaskDetail { get; set; }
+        [Required(ErrorMessage = "Enter Start Date")]
         public Nullable<System.DateTime> StartDate { get; set; }
         public Nullable<System.TimeSpan> StartTime { get; set; }
+        [Required(ErrorMessage = "Enter End Date")]
         public Nullable<System.DateTime> EndDate { get; set; }
         public Nullable<System.TimeSpan> EndTime { get; set; }
         public Nullable<int> Completed { get; set; }
+        [Required(ErrorMessage = "Select Priority")]
         public Nullable<int> PriorityID { get; set; }
 
         public string Priority { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,15 @@ namespace ToDoList.Models
     public class User
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Enter Name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter Username")]
         public string Username { get; set; }
+        [Required(ErrorMessage = "Enter Email")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w)+)+)", ErrorMessage = "Email is not valid")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Enter Password")]
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
         public Nullable<int> Activated { get; set; }
     }
 }
